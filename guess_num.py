09325ed9767181ec0eb61eb68d65791dd7b23054
Moon Chiang <moonlove8310@gmail.com>
2019-06-15 18:09:22 +0800

    add counter

--- guess_num.py
+++ guess_num.py
@@ -1,14 +1,18 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1
     num = input('please guess a number: ')
     num = int(num)
     if num == r:
         print('bingo! congratulations!')
+        print(count, 'time(s) you guessed')
         break
     elif num > r:
         print('your guess is bigger than the target')
     else:
         print('your guess is smaller than the target')
+    print(count, 'time(s) you guessed wrong')
 
